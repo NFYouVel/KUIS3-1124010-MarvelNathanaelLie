b833f795a0fae7091885524ef8e0bfbdfcb0b8db
Marvel Nathanael <marvelnathanael072006@gmail.com>
2025-11-17 13:05:20 +0700

    Update InvalidInputException.java

--- kuis/app/src/main/java/quiz3/models/InvalidInputException.java
+++ kuis/app/src/main/java/quiz3/models/InvalidInputException.java
@@ -2,7 +2,7 @@ package quiz3.models;
 
 import java.util.InputMismatchException;
 
-public class InvalidInputException extends InputMismatchException{
+public class InvalidInputException extends Exception{
 
     public InvalidInputException(String message){
         super("From InvalidInputException: " + message);
