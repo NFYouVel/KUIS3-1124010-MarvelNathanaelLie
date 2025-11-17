ec0645a3e781f401be9f45a93c8b16f7a1166d58
Marvel Nathanael <marvelnathanael072006@gmail.com>
2025-11-17 13:29:22 +0700

    salah tanda doang

--- kuis/app/src/main/java/quiz3/utils/CLIUtil.java
+++ kuis/app/src/main/java/quiz3/utils/CLIUtil.java
@@ -2,6 +2,8 @@ package quiz3.utils;
 
 import java.util.Scanner;
 
+import quiz3.models.InvalidInputException;
+
 public class CLIUtil {
     private static Scanner sc;
 
@@ -12,6 +14,18 @@ public class CLIUtil {
     public static int getInt() {
         return sc.nextInt();
     }
+
+    // public static Number getNum(String message) {
+    //     System.out.println(message);
+    //     String input = sc.nextLine();
+    //     try {
+    //         return Double.parseDouble(input);
+    //     } catch (InvalidInputException e) {
+    //         throw new InvalidInputException("Harus String");
+    //     }
+        
+
+    // }
 
 
 }
